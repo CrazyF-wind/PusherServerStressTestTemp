09778476210285b67053035b96fec228a2e01fb8
Fwind <ff1990@126.com>
2016-09-30 13:58:57 +0800

    新增认证参数querystry

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ var dbhelper = require('./dbtools/dbhelper.js');
 
 for (var socket_n = 0; socket_n < 21; socket_n++) {
 
-    (function() {
+    (function () {
 
         var j = socket_n;
+
+        var token="fixedtoken";
+        var email=process.pid.toString() + "_" + j.toString();
+        var queryStry = "token="+token+"&&email="+email+"&&uniqueID="+email;
         socket = io.connect('http://tmyvitals.ihealthlabs.com.cn:3000', {
-            'force new connection': true
+            transports: ['websocket'], query:queryStry,'force new connection': true
         });
         socket.my_nick = process.pid.toString() + "_" + j.toString();
 
-        (function() {
+        (function () {
             var inner_socket = socket;
-            inner_socket.on('connect', function() {
+            inner_socket.on('connect', function () {
                 console.info("Connected[" + j + "] => " + inner_socket.my_nick);
                 inner_socket.emit('authenticate', {
                     token: "fixedtoken",
@@ -22,9 +26,9 @@ for (var socket_n = 0; socket_n < 21; socket_n++) {
                 });
             });
 
-            inner_socket.on('authenticated', function() {
+            inner_socket.on('authenticated', function () {
                 var interval = Math.floor(Math.random() * 10001) + 5000;
-                setInterval(function() {
+                setInterval(function () {
                     var index = Math.round(Math.random() * 20);
                     var toUser = process.pid.toString() + "_" + index.toString();
                     inner_socket.emit('sendMessage', {
@@ -36,15 +40,15 @@ for (var socket_n = 0; socket_n < 21; socket_n++) {
                     var sendtime = new Date();
                     //详细消息存入数据库
                     var args = {
-                            'from': inner_socket.my_nick,
-                            'to': toUser,
-                            'message': "Regular timer message every " + interval + " ms"
-                        }
-                        // dbhelper.insertMongo('messageDetail', args, function() {
-                        //     console.info("sendtime:" + sendtime.getUTCFullYear() + "-" + (sendtime.getUTCMonth() + 1) + "-" + sendtime.getDate() +
-                        //         " " + sendtime.getUTCHours() + ":" + sendtime.getUTCMinutes() + ":" + sendtime.getUTCSeconds() + "...saved.");
-                        // });
-                        //insert改为upsert保证记录完整
+                        'from': inner_socket.my_nick,
+                        'to': toUser,
+                        'message': "Regular timer message every " + interval + " ms"
+                    }
+                    //dbhelper.insertMongo('messageDetail', args, function() {
+                    //    console.info("sendtime:" + sendtime.getUTCFullYear() + "-" + (sendtime.getUTCMonth() + 1) + "-" + sendtime.getDate() +
+                    //        " " + sendtime.getUTCHours() + ":" + sendtime.getUTCMinutes() + ":" + sendtime.getUTCSeconds() + "...saved.");
+                    //});
+                    //insert改为upsert保证记录完整
                     dbhelper.updateMongoWithOption('messageDetail', args, {
                         $set: {
                             'status': 'send',
@@ -52,30 +56,30 @@ for (var socket_n = 0; socket_n < 21; socket_n++) {
                         }
                     }, {
                         upsert: true
-                    }, function() {
+                    }, function () {
                         console.info('message received')
                     })
 
                     //统计消息存入数据库
                     dbhelper.updateMongoWithOption('messageStatistics', {
-                            'from': inner_socket.my_nick,
-                            'to': toUser
-                        }, {
-                            $inc: {
-                                'send': 1
-                            }
-                        }, {
-                            upsert: true
-                        }, function() {
-                            console.info('sendStatistics saved')
-                        })
-                        // console.info("sendtime:" + sendtime.getUTCFullYear() + "-" + (sendtime.getUTCMonth() + 1) + "-" + sendtime.getDate()
-                        //     + " " + sendtime.getUTCHours() + ":" + sendtime.getUTCMinutes() + ":" + sendtime.getUTCSeconds());
+                        'from': inner_socket.my_nick,
+                        'to': toUser
+                    }, {
+                        $inc: {
+                            'send': 1
+                        }
+                    }, {
+                        upsert: true
+                    }, function () {
+                        console.info('sendStatistics saved')
+                    })
+                    // console.info("sendtime:" + sendtime.getUTCFullYear() + "-" + (sendtime.getUTCMonth() + 1) + "-" + sendtime.getDate()
+                    //     + " " + sendtime.getUTCHours() + ":" + sendtime.getUTCMinutes() + ":" + sendtime.getUTCSeconds());
                 }, interval);
             });
         })();
 
-        socket.on('receiveMessage', function(msg) {
+        socket.on('receiveMessage', function (msg) {
             if (Object.prototype.toString.call(msg) === '[object Array]') {
                 //notifyMe(msg.user,msg.comment);
                 for (var i = 0; i < msg.length; i++) {
@@ -90,34 +94,34 @@ for (var socket_n = 0; socket_n < 21; socket_n++) {
             console.info(msg);
             //收到更新消息后更新消息收到标志位
             dbhelper.updateMongoWithOption('messageDetail', {
-                    'from': msg.from,
-                    'to': msg.to,
-                    //'sendtime': msg.sendDate,
-                    'message': msg.message
-                }, {
-                    $set: {
-                        'status': 'received',
-                        'receivetime': new Date()
-                    }
-                }, {
-                    upsert: true
-                }, function() {
-                    console.info('message received')
-                })
-                //统计消息存入数据库
+                'from': msg.from,
+                'to': msg.to,
+                //'sendtime': msg.sendDate,
+                'message': msg.message
+            }, {
+                $set: {
+                    'status': 'received',
+                    'receivetime': new Date()
+                }
+            }, {
+                upsert: true
+            }, function () {
+                console.info('message received')
+            })
+            //统计消息存入数据库
             dbhelper.updateMongoWithOption('messageStatistics', {
-                    'from': msg.from,
-                    'to': msg.to
-                }, {
-                    $inc: {
-                        'received': 1
-                    }
-                }, {
-                    upsert: true
-                }, function() {
-                    console.info('receivedStatistics saved')
-                })
-                //console.info(msg.from + " said at " + msg.sendDate + " : " + msg.message);
+                'from': msg.from,
+                'to': msg.to
+            }, {
+                $inc: {
+                    'received': 1
+                }
+            }, {
+                upsert: true
+            }, function () {
+                console.info('receivedStatistics saved')
+            })
+            //console.info(msg.from + " said at " + msg.sendDate + " : " + msg.message);
 
             //var args={
             //
@@ -134,7 +138,7 @@ for (var socket_n = 0; socket_n < 21; socket_n++) {
             //});
         }
 
-        socket.on('disconnect', function() {
+        socket.on('disconnect', function () {
             console.info("Disconnected");
         });
     })();
